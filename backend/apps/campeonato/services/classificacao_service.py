2e4ad17e28195abf2b89fdb41abb70e431763b6a
RafaelBarale <rafaelbarale@yahoo.com.br>
2021-07-31 20:58:28 -0300

    service classificacao

--- backend/apps/campeonato/services/classificacao_service.py
+++ backend/apps/campeonato/services/classificacao_service.py
@@ -2,26 +2,26 @@ from django.http.response import Http404
 from ..models import Classificacao
 
 
-def listar_campeonato():
-    return Campeonato.objects.all()
+def listar_classificacao():
+    return Classificacao.objects.all()
 
 
 def cadastrar_campeonato(campeonato_novo):
-    return Campeonato.objects.create(nome=campeonato_novo.nome, temporada=campeonato_novo.temporada)
+    return Classificacao.objects.create(nome=campeonato_novo.nome, temporada=campeonato_novo.temporada)
 
 
-def listar_campeonato_id(id):
+def listar_classificacao_id(id):
     try:
-        return Campeonato.objects.get(id=id)
-    except Campeonato.DoesNotExist:
+        return Classificacao.objects.get(id=id)
+    except Classificacao.DoesNotExist:
         raise Http404
 
 
-def editar_campeonato(campeonato_antigo, campeonato_novo):
+def editar_classificacao(campeonato_antigo, campeonato_novo):
     campeonato_antigo.nome = campeonato_novo.nome
     campeonato_antigo.temporada = campeonato_novo.temporada
     campeonato_antigo.save(force_update=True)
 
 
-def remover_campeonato(campeonato):
+def remover_classificacao(campeonato):
     campeonato.delete()
